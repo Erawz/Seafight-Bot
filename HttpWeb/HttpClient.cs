5a4b1118e4d541e94ed8faefd28e70665922af49
Erawz <47424948+Erawz@users.noreply.github.com>
2019-03-25 22:24:06 +0100

    Add files via upload

---
+++ HttpWeb/HttpClient.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BoxyBot.HttpWeb
+{
+    public class HttpClient
+    {
+        public String UserAgent { get; set; }
+        public CookieContainer Cookies { get; set; }
+
+        public HttpClient()
+        {
+            Cookies = new CookieContainer();
+            UserAgent = "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/52.0.2743.116 Safari/537.36";
+        }
+
+        public String Request(RequestBuilder request, String url)
+        {
+            if (url.StartsWith("http://.", StringComparison.CurrentCulture)) return String.Empty;
+
+            var requestString = request.GetRequestString();
+            var requestBytes = Encoding.UTF8.GetBytes(requestString);
+            url = url + (request.Method == RequestMethod.Get ? "?" + requestString : "");
+            WebRequest webRequest = null;
+            webRequest = WebRequest.Create(url);
+            webRequest.Credentials = CredentialCache.DefaultCredentials;
+            webRequest.Proxy = null;
+            ((HttpWebRequest)webRequest).UserAgent = this.UserAgent;
+            ((HttpWebRequest)webRequest).CookieContainer = Cookies;
+            webRequest.Method = request.Method == RequestMethod.Get ? "GET" : "POST";
+            webRequest.ContentType = "application/x-www-form-urlencoded";
+            Stream dataStream = null;
+            if (request.Method == RequestMethod.Post)
+            {
+                webRequest.ContentLength = requestBytes.Length;
+                dataStream = webRequest.GetRequestStream();
+                dataStream.Write(requestBytes, 0, requestBytes.Length);
+                dataStream.Close();
+            }
+            var response = webRequest.GetResponse();
+            dataStream = response.GetResponseStream();
+            if (dataStream == null) return null;
+            var reader = new StreamReader(dataStream);
+            var responseFromServer = reader.ReadToEnd();
+            reader.Close();
+            dataStream.Close();
+            response.Close();
+            return responseFromServer;
+        }
+    }
+}
---
+++ HttpWeb/RequestBuilder.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace BoxyBot.HttpWeb
+{
+    public class RequestBuilder
+    {
+        public static RequestBuilder Empty = new RequestBuilder(RequestMethod.Get);
+
+        public RequestMethod Method { get; set; }
+
+        private readonly List<KeyValuePair<String, String>> _parameters;
+
+        public RequestBuilder() { _parameters = new List<KeyValuePair<string, string>>(); }
+        public RequestBuilder(RequestMethod method)
+        {
+            _parameters = new List<KeyValuePair<string, string>>();
+            this.Method = method;
+        }
+
+        public void Add(String key, String value) => _parameters.Add(new KeyValuePair<string, string>(key, value));
+
+        public String GetRequestString()
+        {
+            var output = _parameters.Aggregate("", (current, kv) => current + $"{kv.Key}={HttpUtility.UrlEncode(kv.Value)}&");
+            return output.Length > 0 ? output.Remove(output.Length - 1) : String.Empty;
+        }
+
+    }
+}
